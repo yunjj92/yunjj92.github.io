83e0d4c6a4dcc9dd3e63f2ab2a19f39cf1e5625d
KAPANT\yunjj92 <yunjj92@kapanet.or.kr>
2025-02-10 17:55:49 +0900

    feature: progress on adding js function hidelayer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 $ (function(){
 
-
+     let flexContainer = $('div.flex-container');
+     let searchBox = $('.search-box');
+     let searchClose = $('.search-icon-close');
 
     $('.menu-icon, .menu-icon-close').click(function (e){
         e.preventDefault();
         e.stopPropagation();
     });
+
+    function hideLayer(){
+        flexContainer.removeClass('opaque');
+    }
+
 });
